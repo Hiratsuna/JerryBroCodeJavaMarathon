e63fd782d18a91711dda7a87c15575cc01ccb46f
Hiratsuna <adhdmicromanagement@outlook.com>
2023-09-19 13:44:58 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("\tHello and welcome!\n");
+        System.out.println("Hello and welcome!");
+        System.out.print("\"\\Hello and welcome!\\\"\n");
+
+        /* Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        *
+        *This
+        * is
+        * a
+        * comment
+        *
+        *
+         */
+
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+    }
+}
+
