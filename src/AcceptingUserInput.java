5162ee8ac18afb3ac371523052e097a8e6389a22
Hiratsuna <adhdmicromanagement@outlook.com>
2023-09-19 17:10:50 +0300

    1

---
+++ src/AcceptingUserInput.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class AcceptingUserInput {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Swap2Variables.java
+++ src/Swap2Variables.java
@@ -1,5 +1,14 @@
 public class Swap2Variables {
     public static void main(String[] args) {
+       String x = "water";
+       String y = "Kool-Aid";
+       String temp = null;
 
+       temp = x;
+       x = y;
+       y = temp;
+
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
     }
 }
