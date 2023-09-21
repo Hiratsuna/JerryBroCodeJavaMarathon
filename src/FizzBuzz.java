c6b6c188d14a1bc8f5dde2446f08f001498debce
Hiratsuna <adhdmicromanagement@outlook.com>
2023-09-21 08:59:09 +0300

    1

---
+++ src/FizzBuzz.java
@@ -0,0 +1,18 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        String f = "Fizz";
+        String b = "Buzz";
+
+        for (int i = 1;i <= 100 ;i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println(f+' '+b);
+            } else if (i % 3 == 0) {
+                System.out.println(f);
+            } else if (i % 5 == 0) {
+                System.out.println(b);
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
--- src/IfStatements.java
+++ src/IfStatements.java
@@ -1,6 +1,15 @@
 public class IfStatements {
     public static void main(String[] args) {
 
+        int age = 13;
+
+        if (age>=18) {
+            System.out.println("You are an adult!");
+        } else if(age>=75) {
+            System.out.println("Ok, boomer!");
+        } else {
+            System.out.println("You are not an adult!");
+        }
 
     }
 }
